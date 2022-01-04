53959f2330237a54fd32a5acada229981cbdddc5
Christopher <khseredin@phoenixit.ru>
2022-01-04 18:18:41 +0300

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe("App", () => {
+  it('renders learn react link', () => {
+    const app = render(<App />);
+    expect(app).toBeEmptyDOMElement;
+  });
+})
+
+
